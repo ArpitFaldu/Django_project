48466829d1220da795371b6e1e240f60b0b0f9e7
Arpit <falduarpit2004@gmail.com>
2024-04-01 11:43:25 +0530

    Updated finally

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth.models import User,auth
-
+from .models import Device
 # Create your views here.
 def index(request):
-    return render(request,"home.html")
+    mobile=Device.objects.filter(Type="Mobile")
+    laptop=Device.objects.filter(Type="Laptop")
+    return render(request,"home.html",{'mobile':mobile,'laptop':laptop})
--- mobilecomp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MobilecompConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mobilecomp'
